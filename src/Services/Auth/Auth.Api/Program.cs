48e081a6f497272ad2998d440a8519f3ed937f0a
TomasiakM <marcintomasiak00@gmail.com>
2024-07-04 11:51:30 +0200

    Update services registration

--- src/Services/Auth/Auth.Api/Program.cs
+++ src/Services/Auth/Auth.Api/Program.cs
@@ -1,10 +1,23 @@
+using Auth.Application;
+using Auth.Infrastructure;
+using Common.Api;
+using Common.Api.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCommonApi();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services
+    .AddInfrastructure()
+    .AddApplication();
+
 var app = builder.Build();
+
+app.UseMiddleware<GlobalErrorHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
@@ -12,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 app.Run();
