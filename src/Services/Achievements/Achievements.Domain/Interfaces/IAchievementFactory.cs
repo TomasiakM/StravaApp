527b9c0affdda8ef58718edd05b0806ee467976c
TomasiakM <marcintomasiak00@gmail.com>
2024-07-23 11:13:56 +0200

    Add factory interface

---
+++ src/Services/Achievements/Achievements.Domain/Interfaces/IAchievementFactory.cs
@@ -0,0 +1,9 @@
+﻿using Achievements.Domain.Aggregates.Achievement;
+using Achievements.Domain.Aggregates.Achievement.Enums;
+
+namespace Achievements.Domain.Interfaces;
+public interface IAchievementFactory
+{
+    Achievement Create(long stravaUserId, AchievementType achievementType);
+    ICollection<Achievement> CreateAll(long stravaUserId, IEnumerable<Achievement>? without);
+}
