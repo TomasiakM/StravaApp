459ca81de4276e48bcb0704aedea7d720cd976a3
TomasiakM <marcintomasiak00@gmail.com>
2024-08-05 13:09:26 +0200

    Add forgotten service registration

--- src/Services/Strava/Strava.Infrastructure/DependencyInjection.cs
+++ src/Services/Strava/Strava.Infrastructure/DependencyInjection.cs
@@ -3,6 +3,8 @@ using Strava.Application.Interfaces.Services.StravaDataServices;
 using Strava.Infrastructure.Extensions;
 using Strava.Infrastructure.HttpClients;
 using Strava.Infrastructure.Interfaces;
+using Strava.Infrastructure.Interfaces.Auth;
+using Strava.Infrastructure.Services.Auth;
 using Strava.Infrastructure.Services.StravaDataServices;
 
 namespace Strava.Infrastructure;
@@ -16,6 +18,8 @@ public static class DependencyInjection
 
         services.AddScoped<StravaHttpClientService>();
         services.AddScoped<StravaAuthenticationHttpClientService>();
+
+        services.AddScoped<IStravaTokenService, StravaTokenService>();
 
         services.AddScoped<IUserActivityService, UserActivityService>();
         services.AddScoped<IActivityStreamsService, ActivityStreamsService>();
