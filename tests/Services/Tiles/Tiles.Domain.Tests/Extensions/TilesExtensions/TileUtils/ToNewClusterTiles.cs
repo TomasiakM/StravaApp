74533ee535fe37439dbff0b4e003d6eaff61cbf5
TomasiakM <marcintomasiak00@gmail.com>
2024-08-16 11:25:23 +0200

    Add tiles extension tests

---
+++ tests/Services/Tiles/Tiles.Domain.Tests/Extensions/TilesExtensions/TileUtils/ToNewClusterTiles.cs
@@ -0,0 +1,24 @@
+﻿using Tiles.Domain.Aggregates.ActivityTiles.ValueObjects;
+using Tiles.Domain.Extensions.TileExtensions;
+
+namespace Tiles.Domain.Tests.Extensions.TilesExtensions.TileUtils;
+public class ToNewClusterTiles
+{
+    [Fact]
+    public void ShouldReturnNewClusterTilesList()
+    {
+        var tiles = new List<Tile>()
+        {
+            Tile.Create(1, 2),
+            Tile.Create(3, 4),
+            Tile.Create(5, 6),
+        };
+
+        var newClusterTiles = tiles.ToNewClusterTiles();
+
+        Assert.Equal(tiles.Count, newClusterTiles.Count);
+        Assert.True(newClusterTiles.Contains(NewClusterTile.Create(1, 2)));
+        Assert.True(newClusterTiles.Contains(NewClusterTile.Create(3, 4)));
+        Assert.True(newClusterTiles.Contains(NewClusterTile.Create(5, 6)));
+    }
+}
---
+++ tests/Services/Tiles/Tiles.Domain.Tests/Extensions/TilesExtensions/TileUtils/ToNewTiles.cs
@@ -0,0 +1,24 @@
+﻿using Tiles.Domain.Aggregates.ActivityTiles.ValueObjects;
+using Tiles.Domain.Extensions.TileExtensions;
+
+namespace Tiles.Domain.Tests.Extensions.TilesExtensions.TileUtils;
+public class ToNewTiles
+{
+    [Fact]
+    public void ShouldReturnNewTilesList()
+    {
+        var tiles = new List<Tile>()
+        {
+            Tile.Create(1, 2),
+            Tile.Create(3, 4),
+            Tile.Create(5, 6),
+        };
+
+        var newTiles = tiles.ToNewTiles();
+
+        Assert.Equal(tiles.Count, newTiles.Count);
+        Assert.True(newTiles.Contains(NewTile.Create(1, 2)));
+        Assert.True(newTiles.Contains(NewTile.Create(3, 4)));
+        Assert.True(newTiles.Contains(NewTile.Create(5, 6)));
+    }
+}
