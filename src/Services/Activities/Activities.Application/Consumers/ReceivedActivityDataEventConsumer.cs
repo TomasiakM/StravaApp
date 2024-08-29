f719d099295e2d1bd8db343121d10ffd29b0d89c
TomasiakM <marcintomasiak00@gmail.com>
2024-08-29 16:00:34 +0200

    Update consumer

--- src/Services/Activities/Activities.Application/Consumers/ReceivedActivityDataEventConsumer.cs
+++ src/Services/Activities/Activities.Application/Consumers/ReceivedActivityDataEventConsumer.cs
@@ -1,5 +1,7 @@
 ﻿using Activities.Application.Interfaces;
-using Common.MessageBroker.Contracts.Achievements;
+using Activities.Domain.Aggregates.Activities;
+using Activities.Domain.Aggregates.Activities.ValueObjects;
+using Activities.Domain.Aggregates.Streams;
 using Common.MessageBroker.Contracts.Activities;
 using MassTransit;
 using Microsoft.Extensions.Logging;
@@ -23,43 +25,78 @@ public sealed class ReceivedActivityDataEventConsumer
 
     public async Task Consume(ConsumeContext<ReceivedActivityDataEvent> context)
     {
-        _logger.LogInformation("Starting processing activity:{Id} data.", context.Message.Id);
+        _logger.LogInformation("Starting processing activity:{Id}.", context.Message.Id);
 
-        var activity = await _unitOfWork.Activities
-            .GetAsync(e => e.StravaId == context.Message.Id);
-
-        if (activity is null)
-        {
-            var newActivity = _activityAggregateFactory.CreateActivity(context.Message);
-
-            _unitOfWork.Activities.Add(newActivity);
-            await _unitOfWork.SaveChangesAsync();
-
-            _logger.LogInformation("New activity:{Id} \"{Name}\" is created", newActivity.StravaId, newActivity.Name);
-            return;
-        }
-
-        var message = context.Message;
-        activity.Update(
-                message.Name,
-                message.DeviceName,
-                message.SportType,
-                message.Private,
-                message.Distance,
-                message.TotalElevationGain,
-                message.AverageCadence,
-                message.Kilojoules,
-                message.Calories,
-                _activityAggregateFactory.CreateSpeed(message),
-                _activityAggregateFactory.CreateTime(message),
-                _activityAggregateFactory.CreateWatts(message),
-                _activityAggregateFactory.CreateHeartrate(message),
-                _activityAggregateFactory.CreateMap(message));
+        var activity = await CreateOrUpdateActivity(context);
+        await CreateOrUpdateStreams(activity.Id, context);
 
         await _unitOfWork.SaveChangesAsync();
 
-        await _bus.Publish(new UpdateAchievementsEvent(activity.StravaUserId));
+        _logger.LogInformation("Processing activity:{Id} completed.", context.Message.Id);
 
-        _logger.LogInformation("Activity:{Id} \"{Name}\" updated successfully.", activity.StravaId, activity.Name);
+        // add saga pattern to handle tiles and achievements services
+    }
+
+    private async Task<StreamAggregate> CreateOrUpdateStreams(ActivityId activityId, ConsumeContext<ReceivedActivityDataEvent> context)
+    {
+        var streams = await _unitOfWork.Streams
+                    .GetAsync(e => e.ActivityId == activityId);
+
+        if (streams is null)
+        {
+            streams = StreamAggregate.Create(
+                activityId,
+                context.Message.Streams.Cadence,
+                context.Message.Streams.Heartrate,
+                context.Message.Streams.Altitude,
+                context.Message.Streams.Distance,
+                context.Message.Streams.LatLngs);
+
+            _unitOfWork.Streams.Add(streams);
+        }
+        else
+        {
+            streams.Update(
+                context.Message.Streams.Cadence,
+                context.Message.Streams.Heartrate,
+                context.Message.Streams.Altitude,
+                context.Message.Streams.Distance,
+                context.Message.Streams.LatLngs);
+        }
+
+        return streams!;
+    }
+
+    private async Task<ActivityAggregate> CreateOrUpdateActivity(ConsumeContext<ReceivedActivityDataEvent> context)
+    {
+        var activity = await _unitOfWork.Activities
+                    .GetAsync(e => e.StravaId == context.Message.Id);
+
+        if (activity is null)
+        {
+            activity = _activityAggregateFactory.CreateActivity(context.Message);
+            _unitOfWork.Activities.Add(activity);
+        }
+        else
+        {
+            var message = context.Message;
+            activity.Update(
+                    message.Name,
+                    message.DeviceName,
+                    message.SportType,
+                    message.Private,
+                    message.Distance,
+                    message.TotalElevationGain,
+                    message.AverageCadence,
+                    message.Kilojoules,
+                    message.Calories,
+                    _activityAggregateFactory.CreateSpeed(message),
+                    _activityAggregateFactory.CreateTime(message),
+                    _activityAggregateFactory.CreateWatts(message),
+                    _activityAggregateFactory.CreateHeartrate(message),
+                    _activityAggregateFactory.CreateMap(message));
+        }
+
+        return activity;
     }
 }
