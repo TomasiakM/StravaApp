99ff521d125744ace7804cd10c69bbdf047059d3
TomasiakM <marcintomasiak00@gmail.com>
2024-11-18 15:46:08 +0100

    Add consumer tests in athletes service

---
+++ tests/Services/Athletes/Athletes.Application.Tests/Consumers/DeleteUserDetails/Consume.cs
@@ -0,0 +1,37 @@
+﻿using Athletes.Application.Consumers;
+using Athletes.Application.Features.Athletes.Commands.Delete;
+using Common.MessageBroker.Saga.DeleteAllUserdData.Events;
+using Common.MessageBroker.Saga.DeleteAllUserdData.Messages;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Athletes.Application.Tests.Consumers.DeleteUserDetails;
+public class Consume
+{
+    private readonly Mock<ISender> _senderMock = new();
+    private readonly Mock<IBus> _busMock = new();
+
+    [Fact]
+    public async Task ShouldConsumeDeleteUserDetailsMessage()
+    {
+        var stravaUserId = 4;
+        var correlationId = Guid.NewGuid();
+
+        var message = new DeleteUserDetailsMessage(correlationId, stravaUserId);
+
+        var consumer = new DeleteUserDetailsMessageConsumer(
+            _senderMock.Object,
+            _busMock.Object,
+            Mock.Of<ILogger<DeleteUserDetailsMessageConsumer>>());
+
+        var consumeContext = Mock.Of<ConsumeContext<DeleteUserDetailsMessage>>(
+            context => context.Message == message);
+
+        await consumer.Consume(consumeContext);
+
+        _senderMock.Verify(e => e.Send(It.IsAny<DeleteAthleteCommand>(), default), Times.Once);
+        _busMock.Verify(e => e.Publish(It.IsAny<UserDetailsDeletedEvent>(), default), Times.Once);
+    }
+}
---
+++ tests/Services/Athletes/Athletes.Application.Tests/Consumers/ReceivedAthleteData/Consume.cs
@@ -0,0 +1,59 @@
+﻿using Athletes.Application.Consumers;
+using Athletes.Application.Features.Athletes.Commands.Create;
+using Athletes.Application.Features.Athletes.Commands.Update;
+using Athletes.Application.Interfaces;
+using Athletes.Domain.Aggregates.Athletes;
+using Common.MessageBroker.Contracts.Athletes;
+using MassTransit;
+using MediatR;
+using Moq;
+using System.Linq.Expressions;
+
+namespace Athletes.Application.Tests.Consumers.ReceivedAthleteData;
+public class Consume
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+    private readonly Mock<ISender> _senderMock = new();
+
+    [Fact]
+    public async Task ShouldConsumeMessageAndSendCreateAthleteCommandWhenAthleteNotExists()
+    {
+        var message = new ReceivedAthleteDataEvent(1, "test", "first", "last", new DateTime(2022, 1, 1), "profileMedium", "profile");
+
+        var consumer = new ReceivedAthleteDataEventConsumer(_unitOfWorkMock.Object, _senderMock.Object);
+        var consumeContext = Mock.Of<ConsumeContext<ReceivedAthleteDataEvent>>(context =>
+            context.Message == message);
+
+        _unitOfWorkMock
+            .Setup(e => e.Athletes.AnyAsync(It.IsAny<Expression<Func<AthleteAggregate, bool>>>(), default))
+            .ReturnsAsync(false);
+
+        await consumer.Consume(consumeContext);
+
+        _unitOfWorkMock.Verify(e => e.Athletes.AnyAsync(
+            It.IsAny<Expression<Func<AthleteAggregate, bool>>>(), default));
+
+        _senderMock.Verify(e => e.Send(It.IsAny<CreateAthleteCommand>(), default), Times.Once);
+    }
+
+    [Fact]
+    public async Task ShouldConsumeMessageAndSendUpdateAthleteCommandWhenAthleteExists()
+    {
+        var message = new ReceivedAthleteDataEvent(1, "test", "first", "last", new DateTime(2022, 1, 1), "profileMedium", "profile");
+
+        var consumer = new ReceivedAthleteDataEventConsumer(_unitOfWorkMock.Object, _senderMock.Object);
+        var consumeContext = Mock.Of<ConsumeContext<ReceivedAthleteDataEvent>>(context =>
+            context.Message == message);
+
+        _unitOfWorkMock
+            .Setup(e => e.Athletes.AnyAsync(It.IsAny<Expression<Func<AthleteAggregate, bool>>>(), default))
+            .ReturnsAsync(true);
+
+        await consumer.Consume(consumeContext);
+
+        _unitOfWorkMock.Verify(e => e.Athletes.AnyAsync(
+            It.IsAny<Expression<Func<AthleteAggregate, bool>>>(), default));
+
+        _senderMock.Verify(e => e.Send(It.IsAny<UpdateAthleteCommand>(), default), Times.Once);
+    }
+}
