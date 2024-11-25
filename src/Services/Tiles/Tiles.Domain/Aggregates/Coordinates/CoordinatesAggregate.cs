405685b78b01680e4eeb99b4a180cb20ca1df114
TomasiakM <marcintomasiak00@gmail.com>
2024-11-25 11:31:45 +0100

    Update - change empty constructor access modifier to private

--- src/Services/Tiles/Tiles.Domain/Aggregates/Coordinates/CoordinatesAggregate.cs
+++ src/Services/Tiles/Tiles.Domain/Aggregates/Coordinates/CoordinatesAggregate.cs
@@ -26,6 +26,6 @@ public sealed class CoordinatesAggregate : AggregateRoot<CoordinatesId>
     }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public CoordinatesAggregate() : base(CoordinatesId.Create()) { }
+    private CoordinatesAggregate() : base(CoordinatesId.Create()) { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
