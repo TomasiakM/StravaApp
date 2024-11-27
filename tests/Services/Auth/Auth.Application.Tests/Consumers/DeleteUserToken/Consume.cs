af4d933f3751e2764c9fe52ca329987f136600d6
TomasiakM <marcintomasiak00@gmail.com>
2024-11-27 22:25:50 +0100

    Add delete user token message consumer test

---
+++ tests/Services/Auth/Auth.Application.Tests/Consumers/DeleteUserToken/Consume.cs
@@ -0,0 +1,36 @@
+﻿using Auth.Application.Consumers;
+using Auth.Application.Features.Token.Commands.Delete;
+using Common.MessageBroker.Saga.DeleteAllUserdData.Events;
+using Common.MessageBroker.Saga.DeleteAllUserdData.Messages;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Auth.Application.Tests.Consumers.DeleteUserToken;
+public class Consume
+{
+    private readonly Mock<ISender> _senderMock = new();
+    private readonly Mock<IBus> _busMock = new();
+
+    [Fact]
+    public async Task ShouldSendDeleteUserTokenCommand()
+    {
+        var correlationId = Guid.NewGuid();
+        var userId = 1;
+        var message = new DeleteUserTokenMessage(correlationId, userId);
+
+        var consumer = new DeleteUserTokenMessageConsumer(
+            _senderMock.Object,
+            _busMock.Object,
+            Mock.Of<ILogger<DeleteUserTokenMessageConsumer>>());
+
+        var consumeContext = Mock.Of<ConsumeContext<DeleteUserTokenMessage>>(context =>
+            context.Message == message);
+
+        await consumer.Consume(consumeContext);
+
+        _senderMock.Verify(e => e.Send(new DeleteTokenCommand(message.StravaUserId), default), Times.Once);
+        _busMock.Verify(e => e.Publish(new UserTokenDeletedEvent(message.CorrelationId, message.StravaUserId), default), Times.Once);
+    }
+}
