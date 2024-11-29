622261367ef2d294e22c82b945bdfad7232a4916
TomasiakM <marcintomasiak00@gmail.com>
2024-11-29 12:48:36 +0100

    Add consumers tests in strava service

---
+++ tests/Services/Strava/Strava.Application.Tests/Consumers/FetchAthleteActivity/Consume.cs
@@ -0,0 +1,76 @@
+﻿using Common.Domain.Enums;
+using Common.MessageBroker.Contracts.Activities;
+using Common.MessageBroker.Saga.ProcessActivityData;
+using Common.Tests.Utils;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Strava.Application.Consumers;
+using Strava.Application.Interfaces.Services.StravaDataServices;
+using Strava.Application.Models;
+using Strava.Contracts.Activity;
+
+namespace Strava.Application.Tests.Consumers.FetchAthleteActivity;
+public class Consume
+{
+    private readonly Mock<IUserActivityService> _userActivityServiceMock = new();
+    private readonly Mock<IActivityStreamsService> _activityStreamsServiceMock = new();
+    private readonly Mock<IBus> _busMock = new();
+
+    [Fact]
+    public async Task ShouldPublishProcessActivityDataMessage()
+    {
+        var activity = new StravaActivityDetailsResponse(1, "2", 3, 4, 5, 6, SportType.AlpineSki,
+            new(), new(), new float[] { 0, 0 }, new float[] { 0, 0 }, true, 7, 8, 9, 10, 11,
+            true, 12, 13, "14", true, 15, 16, new(17), new("18", "19", "20"));
+        var streams = new ActivityStreams(new(), new(), new(), new(), new(), new());
+
+        var message = new FetchAthleteActivityEvent(activity.Athlete.Id, activity.Id);
+
+        var consumer = new FetchAthleteActivityEventConsumer(
+            Mock.Of<ILogger<FetchAthleteActivityEventConsumer>>(),
+            _userActivityServiceMock.Object,
+            _activityStreamsServiceMock.Object,
+            _busMock.Object,
+            MapperFactory.Create(typeof(FetchAthleteActivityEventConsumer).Assembly));
+
+        var consumeContext = Mock.Of<ConsumeContext<FetchAthleteActivityEvent>>(context =>
+            context.Message == message);
+
+        _userActivityServiceMock
+            .Setup(e => e.GetAsync(message.StravaUserId, message.StravaActivityId, default))
+            .ReturnsAsync(activity);
+
+        _activityStreamsServiceMock
+            .Setup(e => e.GetAsync(message.StravaUserId, message.StravaActivityId, default))
+            .ReturnsAsync(streams);
+
+        await consumer.Consume(consumeContext);
+
+        _busMock.Verify(e => e.Publish(It.IsAny<ProcessActivityDataMessage>(), default), Times.Once);
+    }
+
+    [Fact]
+    public async Task ShouldNotPublishProcessActivityDataMessage()
+    {
+        var message = new FetchAthleteActivityEvent(1, 2);
+
+        var consumer = new FetchAthleteActivityEventConsumer(
+            Mock.Of<ILogger<FetchAthleteActivityEventConsumer>>(),
+            _userActivityServiceMock.Object,
+            _activityStreamsServiceMock.Object,
+            _busMock.Object,
+            MapperFactory.Create(typeof(FetchAthleteActivityEventConsumer).Assembly));
+
+        var consumeContext = Mock.Of<ConsumeContext<FetchAthleteActivityEvent>>(context =>
+            context.Message == message);
+
+        _userActivityServiceMock
+            .Setup(e => e.GetAsync(message.StravaUserId, message.StravaActivityId, default))
+            .ReturnsAsync((StravaActivityDetailsResponse?)null);
+
+        await consumer.Consume(consumeContext);
+
+        _busMock.Verify(e => e.Publish(It.IsAny<ProcessActivityDataMessage>(), default), Times.Never);
+    }
+}
---
+++ tests/Services/Strava/Strava.Application.Tests/Consumers/NewAthleteLoggedIn/Consume.cs
@@ -0,0 +1,56 @@
+﻿using Common.MessageBroker.Contracts.Activities;
+using Common.MessageBroker.Contracts.Athletes;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Strava.Application.Consumers;
+using Strava.Application.Interfaces.Services.StravaDataServices;
+using Strava.Contracts.Activity;
+
+namespace Strava.Application.Tests.Consumers.NewAthleteLoggedIn;
+public class Consume
+{
+    private readonly Mock<IAllUserActivitiesService> _allUserActivitiesServiceMock = new();
+    private readonly Mock<IBus> _busMock = new();
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public async Task ShouldPublishFetchAthleteActivityEventForAllActivities(List<StravaActivitySummaryResponse> activities)
+    {
+        var message = new NewAthleteLoggedInEvent(1);
+
+        var consumer = new NewAthleteLoggedInEventConsumer(
+            Mock.Of<ILogger<NewAthleteLoggedInEventConsumer>>(),
+            _allUserActivitiesServiceMock.Object,
+            _busMock.Object);
+
+        var consumeContext = Mock.Of<ConsumeContext<NewAthleteLoggedInEvent>>(context =>
+            context.Message == message);
+
+        _allUserActivitiesServiceMock
+            .Setup(e => e.GetAsync(message.StravaUserId, default))
+            .ReturnsAsync(activities);
+
+        await consumer.Consume(consumeContext);
+
+        _busMock.Verify(e => e.Publish(It.IsAny<FetchAthleteActivityEvent>(), default), Times.Exactly(activities.Count));
+    }
+
+    public static IEnumerable<object[]> TestCases()
+    {
+        yield return new object[] { new List<StravaActivitySummaryResponse>() };
+
+        yield return new object[] { new List<StravaActivitySummaryResponse>
+        {
+            new(1, new(), new(2)),
+        }};
+
+        yield return new object[] { new List<StravaActivitySummaryResponse>
+        {
+            new(1, new(), new(2)),
+            new(2, new(), new(2)),
+            new(3, new(), new(2)),
+            new(4, new(), new(2))
+        }};
+    }
+}
