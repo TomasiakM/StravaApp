b1d569fd6d54ba7c67b765eeb01b148a760ce81f
TomasiakM <marcintomasiak00@gmail.com>
2025-02-20 13:11:00 +0100

    Add achivements consumers tests

---
+++ tests/Services/Achievements/Achievements.Integration.Tests/Consumers/Application/DeleteUserAchievements.cs
@@ -0,0 +1,24 @@
+﻿using Achievements.Application.Consumers;
+using Common.MessageBroker.Saga.DeleteAllUserdData.Events;
+using Common.MessageBroker.Saga.DeleteAllUserdData.Messages;
+
+namespace Achievements.Integration.Tests.Consumers.Application;
+
+public class DeleteUserAchievements : BaseTest, IClassFixture<IntegrationTestWebAppFactory>
+{
+    public DeleteUserAchievements(IntegrationTestWebAppFactory factory)
+        : base(factory) { }
+
+    [Fact]
+    public async Task ConsumerShould_ConsumeAndPublishEvent()
+    {
+        var message = new DeleteUserAchievementsMessage(Guid.NewGuid(), 1);
+
+        await Harness.Bus.Publish(message);
+        await Task.Delay(50);
+
+        var consumerHarness = Harness.GetConsumerHarness<DeleteUserAchievementsMessageConsumer>();
+        Assert.True(await consumerHarness.Consumed.Any<DeleteUserAchievementsMessage>());
+        Assert.True(await Harness.Published.Any<UserAchievementsDeletedEvent>());
+    }
+}
---
+++ tests/Services/Achievements/Achievements.Integration.Tests/Consumers/Application/UpdateAchievements.cs
@@ -0,0 +1,32 @@
+﻿using Achievements.Application.Consumers;
+using Common.Domain.Models;
+using Common.MessageBroker.Contracts.Activities.GetUserActivities;
+using Common.MessageBroker.Saga.Common.Events;
+using Common.MessageBroker.Saga.Common.Messages;
+using MassTransit;
+
+namespace Achievements.Integration.Tests.Consumers.Application;
+
+public class UpdateAchievements : BaseTest, IClassFixture<IntegrationTestWebAppFactory>
+{
+    public UpdateAchievements(IntegrationTestWebAppFactory factory)
+        : base(factory) { }
+
+    [Fact]
+    public async Task ConsumerShould_ConsumeAndPublishEvent()
+    {
+        var message = new UpdateAchievementsMessage(Guid.NewGuid(), 1, 1);
+
+        Harness.Bus.ConnectReceiveEndpoint(
+            e => e.Handler<GetUserActivitiesRequest>(
+                async e => await e.RespondAsync(
+                    new GetUserActivitiesResponse(new List<Activity>()))));
+
+        await Harness.Bus.Publish(message);
+        await Task.Delay(50);
+
+        var consumerHarness = Harness.GetConsumerHarness<UpdateAchievementsMessageConsumer>();
+        Assert.True(await consumerHarness.Consumed.Any<UpdateAchievementsMessage>());
+        Assert.True(await Harness.Published.Any<AchievementsUpdatedEvent>());
+    }
+}
