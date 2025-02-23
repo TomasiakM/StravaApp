c2cbb8251cfd26c4df40aa3b599e58b2775c131f
TomasiakM <marcintomasiak00@gmail.com>
2025-02-23 20:02:14 +0100

    Add athlete integration tests project

---
+++ tests/Services/Athletes/Athletes.Integration.Tests/BaseTest.cs
@@ -0,0 +1,53 @@
+﻿using Athletes.Infrastructure.Persistence;
+using Common.Tests;
+using Common.Tests.Utils;
+using MassTransit.Testing;
+using MediatR;
+
+namespace Athletes.Integration.Tests;
+
+[Trait("Category", "Integration")]
+[Collection(nameof(IntegrationTestCollection<IntegrationTestWebAppFactory>))]
+public abstract class BaseTest : IAsyncLifetime
+{
+    private protected readonly ServiceDbContext Db;
+    protected HttpClient ServiceClient;
+    protected Func<Task> ResetDb;
+    protected ITestHarness Harness;
+    protected IMediator Mediator;
+
+    public BaseTest(IntegrationTestWebAppFactory factory)
+    {
+        Db = factory.Db;
+
+        Mediator = factory.Mediator;
+
+        ResetDb = factory.ResetDatabase;
+
+        Harness = factory.Services.GetTestHarness();
+        ServiceClient = factory.CreateClient();
+    }
+
+    public async Task Insert<T>(T entity) where T : class
+    {
+        Db.Add(entity);
+        await Db.SaveChangesAsync();
+        Db.ChangeTracker.Clear();
+    }
+
+    public void AddToken(int userId = 1)
+    {
+        var token = TestTokenGenerator.Create(userId);
+        ServiceClient.DefaultRequestHeaders.Authorization = new("Bearer", token);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await ResetDb();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await ResetDb();
+    }
+}
---
+++ tests/Services/Athletes/Athletes.Integration.Tests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,72 @@
+﻿using Athletes.Infrastructure.Persistence;
+using Common.Tests.Extensions;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Respawn;
+using System.Data.Common;
+using Testcontainers.MsSql;
+
+namespace Athletes.Integration.Tests;
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly MsSqlContainer _container = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithPassword("P@ssw0rd")
+        .WithCleanUp(true)
+        .Build();
+
+    internal ServiceDbContext Db { get; private set; } = null!;
+    private Respawner _respawner = null!;
+    private DbConnection _connection = null!;
+    internal IMediator Mediator = null!;
+
+    public async Task ResetDatabase()
+    {
+        await _respawner.ResetAsync(_connection);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _container.StartAsync();
+
+        var scope = Services.CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+
+        Db = serviceProvider.GetRequiredService<ServiceDbContext>();
+        Mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        _connection = Db.Database.GetDbConnection();
+        await _connection.OpenAsync();
+
+        _respawner = await Respawner.CreateAsync(_connection, new RespawnerOptions
+        {
+            DbAdapter = DbAdapter.SqlServer,
+            SchemasToInclude = new[] { "dbo" }
+        });
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _connection.CloseAsync();
+        await _container.DisposeAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveDbContext<ServiceDbContext>();
+            services.AddDbContext<ServiceDbContext>(options =>
+            {
+                options.UseSqlServer(_container.GetConnectionString());
+            });
+            services.EnsureDbCreated<ServiceDbContext>();
+            services.AddMassTransitTestHarness();
+        });
+    }
+}
